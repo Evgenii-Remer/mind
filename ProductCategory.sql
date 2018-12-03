7a6a3035be24f5e8db00d1f50a34b41389ef6745
Evgenii-Remer <45551815+Evgenii-Remer@users.noreply.github.com>
2018-12-03 09:07:16 +0300

    Add files via upload

---
+++ ProductCategory.sql
@@ -0,0 +1,8 @@
+﻿SELECT 
+ prod.ProductId,
+ ,prod.Name AS ProductName
+ ,cat.Name  AS CategoryName
+ FROM Product prod
+  LEFT JOIN ProductCategoryLink lnk ON lnk.ProductId = prod.ProductId
+  LEFT JOIN Category cat ON lnk.CategoryId = cat.CategoryId
+  
